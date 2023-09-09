f309e7ce543bd8f5295cc1cceaf7f0377a3cf2c6
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-09-09 15:08:37 +0200

    Layout bg foundations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import "./scss/style.scss";
+import bgImageDesktop from "./assets/bg-main-desktop.png";
+import bgImageMobile from "./assets/bg-main-mobile.png";
+
 function App() {
   return (
     <>
-      <div>hello</div>
+      <main>
+        <div className="bg-main-background">
+          <img
+            src={bgImageDesktop}
+            alt=""
+            className="bg-main-background-desktop"
+          />
+          <img
+            src={bgImageMobile}
+            alt=""
+            className="bg-main-background-mobile"
+          />
+        </div>
+        <div>2 content</div>
+      </main>
     </>
   );
 }
