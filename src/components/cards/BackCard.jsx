859dfb10af0183f5aa71fff5f101af69ad8d0cd3
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-09-10 14:54:45 +0200

    Card comps with respective image

---
+++ src/components/cards/BackCard.jsx
@@ -0,0 +1,12 @@
+import image from "../../assets/bg-card-back.png";
+
+function BackCard() {
+  return (
+    <img
+      src={image}
+      alt=""
+    />
+  );
+}
+
+export default BackCard;
---
+++ src/components/cards/FrontCard.jsx
@@ -0,0 +1,12 @@
+import image from "../../assets/bg-card-front.png";
+
+function FrontCard() {
+  return (
+    <img
+      src={image}
+      alt=""
+    />
+  );
+}
+
+export default FrontCard;
