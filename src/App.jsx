6e7be7c70d501726759d35e6f20029062fed894f
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-09-19 21:07:25 +0200

    Nested components in a context provider component, and changed out form to formContainer, which now contains two comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./scss/style.scss";
 import Background from "./components/Background";
-import Form from "./components/Form";
-import CardContainer from "./components/CardContainer";
+import FormContainer from "./components/form/FormContainer";
+import CardContainer from "./components/cards/CardContainer";
 import FrontCard from "./components/cards/FrontCard";
 import BackCard from "./components/cards/BackCard";
+import { FormProvider } from "./context/FormContext";
 
 function App() {
   return (
     <>
       <main>
         <Background />
-        <CardContainer>
-          <FrontCard />
-          <BackCard />
-        </CardContainer>
-        <Form />
+        <FormProvider>
+          <CardContainer>
+            <FrontCard />
+            <BackCard />
+          </CardContainer>
+          <FormContainer />
+        </FormProvider>
       </main>
     </>
   );
