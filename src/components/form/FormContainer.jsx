2649ec6729a362eb007b8668a96ff907e477ede6
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-09-30 12:14:47 +0200

    turbo console test

--- src/components/form/FormContainer.jsx
+++ src/components/form/FormContainer.jsx
@@ -4,6 +4,13 @@ import FormSuccess from "./FormSuccess";
 
 function FormContainer() {
   const [complete, setComplete] = useState(false);
+
+  const de = {
+    name: "jajaja",
+    surname: "neineineni",
+    age: 23,
+  };
+
   return <>{complete ? <FormSuccess setComplete={setComplete} /> : <Form setComplete={setComplete} />}</>;
 }
 
