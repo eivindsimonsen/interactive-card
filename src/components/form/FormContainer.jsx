160d3a1a617a234ab289d343ca424ce4a96d8dab
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-09-30 12:22:18 +0200

    Removed some test code

--- src/components/form/FormContainer.jsx
+++ src/components/form/FormContainer.jsx
@@ -4,13 +4,6 @@ import FormSuccess from "./FormSuccess";
 
 function FormContainer() {
   const [complete, setComplete] = useState(false);
-
-  const de = {
-    name: "jajaja",
-    surname: "neineineni",
-    age: 23,
-  };
-
   return <>{complete ? <FormSuccess setComplete={setComplete} /> : <Form setComplete={setComplete} />}</>;
 }
 
